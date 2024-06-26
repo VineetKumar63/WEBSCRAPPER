0d76e028ac4ea3c9d2c1b8daca93eca1bc16e715
vineet <vvineet.kumar21@gmail.com>
2024-06-26 06:32:48 +0530

    first commit

---
+++ components/Searchbar.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+
+const Searchbar = () => {
+
+    const handleSubmit = () => {
+
+    }
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-12"></form>
+  )
+}
+
+export default Searchbar
