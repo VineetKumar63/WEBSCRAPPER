ca4ed8433fdbc7fd2dcbe7da5257a3bc99593054
vineet <vvineet.kumar21@gmail.com>
2024-07-03 11:15:36 +0530

    new modules add . product by id

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import HeroCarousel from '@/components/HeroCarousel'
+import ProductCard from '@/components/ProductCard'
 import Searchbar from '@/components/Searchbar'
+import { getAllProducts } from '@/lib/actions'
 import Image from 'next/image'
 import React from 'react'
+const Home = async () => {
+  const allProducts = await getAllProducts();
 
-const Home = () => {
   return (
     <>
    <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
@@ -16,16 +20,15 @@ const Home = () => {
       <p className='mt-6'> Powerful, self-serve product and growth analytics to help you convert, engage, and retain more. </p>
       <Searchbar/>
     </div>
-    HeroCarousel
+    <HeroCarousel/>
    </section>
    <section className='trending-section'>
     <h2 className='section-text'>Trending</h2>
     <div className='flex flex-wrap gap-x-8 gap-y-16
-    '>{['Apple Iphone 15', 'Book', 'Sneekers'].map((product) => (
-      <div>{product}</div>
+    '>{allProducts?.map((product) => (
+      <ProductCard key={product._id} product={product}/>
     ))}
       </div>
-
    </section>
    </>
   )
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,14 +1,47 @@
 'use client'
-import React from 'react'
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import React, { FormEvent, useState } from 'react'
 
+const isValidAmazonProductURL = (url: string) => {
+  try{
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+    if(hostname.includes('amazon.in')|| hostname.includes('amazon.')||hostname.endsWith('amzon')){
+      return true;
+    }}
+  catch(error){
+    return false;
+  } 
+  return false;
+}
 
 const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+    const handleSubmit = async (event : FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-    const handleSubmit = () => {
-
-    }
+      if(!isValidLink) return alert('Please provide a valid Amazon link')
+        try {
+          setIsLoading(true);
+          const product = await scrapeAndStoreProduct(searchPrompt);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    
   return (
-    <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-12"></form>
+    <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-12">
+      <input type="text" value={searchPrompt} 
+      onChange={(e)=> setSearchPrompt(e.target.value)}
+      className='searchbar-input'  placeholder='enter product link'/>
+      <button type='submit' className='searchbar-btn' disabled={searchPrompt===''}> {isLoading ? 'Searching...' : 'Search' }
+      </button>
+    </form>
+
   )
 }
 
